6224095674ee38fe57d3815ce6642d8af1668ce1
Wlpsensor7 <2642195167@qq.com>
2018-07-27 20:29:16 +0800

    修改根url

--- bbs/urls.py
+++ bbs/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/',include('APP.urls',namespace='app')),
+    url(r'^bbs/',include('APP.urls',namespace='app')),
 ]
