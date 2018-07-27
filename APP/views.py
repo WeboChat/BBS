e4b34012662556c94c8d8b06475b32e4bba3131d
Wlpsensor7 <2642195167@qq.com>
2018-07-27 20:12:15 +0800

    在app中添加url 创建基本的视图函数 将app中的url添加到根url中

--- APP/views.py
+++ APP/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def hello(request):
+    return JsonResponse('hello bbs')
